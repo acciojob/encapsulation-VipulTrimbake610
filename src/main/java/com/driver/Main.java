1eb65f43a8f5f2b2bbba182dcde7aeab7ccbe504
VipulTrimbake610 <vipultrimbake610@gmail.com>
2024-04-09 21:06:01 +0530

    First

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        RWOnly rwonly = new RWOnly();
+//        RWOnly.name = "5";
+//        name has private access in com.driver.RWOnly
+        rwonly.setName("Hello");
+        System.out.println(rwonly.getName());
+    }
 }
